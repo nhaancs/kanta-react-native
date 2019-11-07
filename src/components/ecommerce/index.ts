bdde345d05c2d2860fded806b235dc79b29f6fd6
Nhan NQ <nhannq@stageit.app>
2019-11-07 12:57:25 +0700

    - refactor 2

--- src/components/ecommerce/index.ts
+++ src/components/ecommerce/index.ts
@@ -3,35 +3,6 @@ export {
   EcommerceHeaderProps,
 } from './ecommerce.header';
 export {
-  AddPaymentCardForm,
-  AddPaymentCardFormProps,
-  AddPaymentCardFormType,
-} from './addPaymentCardForm.component';
-export {
-  ApartmentFacilityList1,
-  ApartmentFacilityList1Props,
-} from './apartmentFacilityList1.component';
-export {
-  ApartmentFacilityList2,
-  ApartmentFacilityList2Props,
-} from './apartmentFacilityList2.component';
-export {
-  ApartmentPhotoList,
-  ApartmentPhotoListProps,
-} from './apartmentPhotoList.component';
-export {
-  PriceText,
-  PriceTextProps,
-} from './priceText.component';
-export {
-  PaymentCard,
-  PaymentCardProps,
-} from './paymentCard.component';
-export {
-  CartProduct,
-  CartProductProps,
-} from './cartProduct.component';
-export {
   ProductInfo,
   ProductInfoProps,
 } from './productInfo.component';
